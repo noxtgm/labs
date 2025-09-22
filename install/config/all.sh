582aa95640fb640d96c266717fd57d3df1eb7307
noxtgm <contact@noxtgm.me>
2025-09-22 12:59:36 -0200

    Remove Apple & mac support

--- install/config/all.sh
+++ install/config/all.sh
@@ -14,16 +14,11 @@ run_logged $OMARCHY_INSTALL/config/fix-powerprofilesctl-shebang.sh
 run_logged $OMARCHY_INSTALL/config/docker.sh
 run_logged $OMARCHY_INSTALL/config/mimetypes.sh
 run_logged $OMARCHY_INSTALL/config/localdb.sh
-run_logged $OMARCHY_INSTALL/config/sudoless-asdcontrol.sh
 run_logged $OMARCHY_INSTALL/config/hardware/network.sh
 run_logged $OMARCHY_INSTALL/config/hardware/set-wireless-regdom.sh
-run_logged $OMARCHY_INSTALL/config/hardware/fix-fkeys.sh
 run_logged $OMARCHY_INSTALL/config/hardware/bluetooth.sh
 run_logged $OMARCHY_INSTALL/config/hardware/printer.sh
 run_logged $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 run_logged $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
-run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-bcm4360.sh
-run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-spi-keyboard.sh
-run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-t2.sh
