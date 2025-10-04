e771a5a88bde83534fa79471596d0b0ad8fea82b
noxtgm <contact@noxtgm.me>
2025-10-05 00:40:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -eEo pipefail
 
+# Repository variables
+REPOSITORY_NAME="labs"
+
 # Define locations
-export REPOSITORY_PATH="$HOME/.local/share/labs"
+export REPOSITORY_PATH="$HOME/.local/share/$REPOSITORY_NAME"
 export REPOSITORY_INSTALL="$REPOSITORY_PATH/install"
