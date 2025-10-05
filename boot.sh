43d1dca17d1d5c22ad8cd0e45109a3abd370b870
noxtgm <contact@noxtgm.me>
2025-10-05 02:13:00 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -84,22 +84,22 @@ rm -rf ~/.local/share/labs/
 sleep 2
 echo -e "\n${GREEN}[SUCCESS] Agent authentication successful${NC}"
 sleep 2
-echo -e "\n${BLUE}[INFO] Initializing secure communication protocols...${NC}"
+echo -e "${BLUE}[INFO] Initializing secure communication protocols...${NC}"
 sleep 2
-echo -e "\n${BLUE}[INFO] Establishing encrypted connection...${NC}"
+echo -e "${BLUE}[INFO] Establishing encrypted connection...${NC}"
 sleep 4
 
 clear
 
 # Clone the default repository unless a specific repository is specified
-echo -e "${BLUE}[INFO] Retrieving system components from:${NC} ${BOLD_WHITE}https://github.com/${REPO_AUTHOR}/${REPO_NAME}.git${NC}"
+echo -e "${BLUE}[INFO] Retrieving system components from:${NC} ${BOLD_WHITE}https://github.com/${REPO_AUTHOR}/${REPO_NAME}.git${NC}\n"
 git clone "https://github.com/${REPO_AUTHOR}/${REPO_NAME}.git" ~/.local/share/labs >/dev/null
 
 sleep 2
 
 # Switch to the default branch unless a specific branch is specified
 if [[ $REPO_REF != "main" ]]; then
-    echo -e "\n${BLUE}[INFO] Switching to operational branch:${NC} ${BOLD_WHITE}$REPO_REF${NC}"
+    echo -e "\n${BLUE}[INFO] Switching to operational branch:${NC} ${BOLD_WHITE}$REPO_REF${NC}\n"
     cd ~/.local/share/labs
     git fetch origin "${REPO_REF}" && git checkout "${REPO_REF}"
     cd -
