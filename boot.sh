4662f78fba05b170a88cb03887ab8092a2f0c1de
noxtgm <contact@noxtgm.me>
2025-11-06 14:34:56 +0100

    Update boot and install logic

--- boot.sh
+++ boot.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Repository variables
-REPO_REF="${REPO_REF:-main}"
-REPO_NAME="${REPO_NAME:-labs}"
-REPO_AUTHOR="${REPO_AUTHOR:-noxtgm}"
+# Define environment variables
+export REPO_REF="${REPO_REF:-main}"
+export REPO_NAME="${REPO_NAME:-labs}"
+export REPO_AUTHOR="${REPO_AUTHOR:-noxtgm}"
 
-# Colors
-NC='\033[0m'
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BOLD_WHITE='\033[1;37m'
+# Define environment locations
+export REPO_PATH="${HOME}/.local/share/${REPO_NAME}"
+export REPO_INSTALL="${REPO_PATH}/install"
+export REPO_INSTALL_LOG="${REPO_PATH}/install.log"
 
-# ASCII logo
-ascii_logo='
+# Define colors
+export NC='\033[0m'
+export RED='\033[0;31m'
+export BLUE='\033[0;34m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export BOLD_WHITE='\033[1;37m'
+
+# Define ASCII logo
+export ascii_logo='
                                                   &&&&&&&&&                                                  
                                         &&&&&&X;;;;: ;;;&&&&&&&&&&&&&                                        
                                    &&&&  ;;;;;;;;;   ;;&&&&&&&&&X;;;;;&&&&                                   
@@ -44,7 +49,7 @@ ascii_logo='
    &&&&&&&&&&&&&&&    ;;;;;;;;;;;;;;;;;;;;;;;;'${RED}'$XXXXXXXXXXXXXXX'${NC}';;;;;;;;;   ;;;;;;;;;                  ;;;;&   
   &&&&&&&&&&&&&&&&&&&&&&&&x;;;;;;;;;;;;;;;;;;'${RED}'$XXXXXXXXXXXXXXXXX'${NC}';;;;;;;;;; ;;;;;;                      ;;;&   
   &&   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'${RED}'$XXXXXXXXXXXXXXXXX'${NC}';;;;;;;;;;;;;                          ;;;&   
-  &&                   ;;;;;;;;;;;;;;;;;;;;;;'${RED}'&XXXXXXXXXXXXXXXXX'${NC}'+$&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&X;&   
+  &&                   ;;;;;;;;;;;;;;;;;;;;;;'${RED}'&XXXXXXXXXXXXXXXXX'${NC}'$&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&X;&   
    &                    ;;;;;;;;;;;;;;;;;;;;;;'${RED}'XXXXXXXXXXXXXXXXx'${NC}';;;;;;;;;;;;;;;;;;    &&&&&&&&&&&&&&&&&&&&&   
    &                     ;;;;;;;;;;;;;;;;;;;;;;'${RED}'XXXXXXXXXXXXXXx'${NC}';;;;;;;;;;;;;;;;;;;            &&&&&&&&&&&&&   
    &&                      ;;;;;;;;;     ;;;;;&;;'${RED}'XXXXXXXXXX+'${NC}';;;;;;;;;;;;;;;;;;;;                  &&&&&&&&   
@@ -76,11 +81,13 @@ ascii_logo='
 
 clear
 
+# Remove previous installation if it exists
 echo -e "\n$ascii_logo\n"
 echo -e "${BOLD_WHITE}Welcome agent, please enter your password to begin the installation process${NC}"
 sudo pacman -Syu --noconfirm --needed git >/dev/null
-rm -rf ~/.local/share/labs/
+rm -rf "${REPO_PATH}"
 
+# Lore dump
 sleep 2
 echo -e "\n${GREEN}[SUCCESS] Agent authentication successful${NC}"
 sleep 2
@@ -88,32 +95,25 @@ echo -e "${BLUE}[INFO] Initializing secure communication protocols...${NC}"
 sleep 2
 echo -e "${BLUE}[INFO] Establishing encrypted connection...${NC}"
 sleep 4
-
 clear
 
-# Clone the default repository unless a specific repository is specified
+# Clone the repository
 echo -e "${BLUE}[INFO] Retrieving system components from:${NC} ${BOLD_WHITE}https://github.com/${REPO_AUTHOR}/${REPO_NAME}.git${NC}\n"
-git clone "https://github.com/${REPO_AUTHOR}/${REPO_NAME}.git" ~/.local/share/labs >/dev/null
-
+git clone "https://github.com/${REPO_AUTHOR}/${REPO_NAME}.git" "${REPO_PATH}" >/dev/null
 sleep 2
 
-# Switch to the default branch unless a specific branch is specified
-if [[ $REPO_REF != "main" ]]; then
-    echo -e "\n${BLUE}[INFO] Switching to operational branch:${NC} ${BOLD_WHITE}$REPO_REF${NC}\n"
-    cd ~/.local/share/labs
-    git fetch origin "${REPO_REF}" && git checkout "${REPO_REF}"
-    cd -
-fi
+# Switch to the specified branch
+echo -e "\n${BLUE}[INFO] Switching to operational branch:${NC} ${BOLD_WHITE}${REPO_REF}${NC}\n"
+cd "${REPO_PATH}"
+git fetch origin "${REPO_REF}" && git checkout "${REPO_REF}"
+cd -
 
+# Lore dump
 echo -e "\n${GREEN}[SUCCESS] Repository secured. Beginning system deployment...${NC}"
-
 sleep 4
-
 clear
-
 echo -e "${YELLOW}[WARNING] This system contains classified information. Unauthorized access is prohibited.${NC}\n"
-
 sleep 3
 
 # Begin the installation process
-source ~/.local/share/labs/install.sh
+source "${REPO_PATH}/install.sh"
--- install.sh
+++ install.sh
@@ -3,15 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -eEo pipefail
 
-# Repository variables
-export REPO_NAME="labs"
-
-# Define locations
-export REPO_PATH="$HOME/.local/share/$REPO_NAME"
-export REPO_INSTALL="$REPO_PATH/install"
-export REPO_INSTALL_LOG_FILE="$HOME/.local/share/$REPO_NAME/install.log"
-
-# Install
+# Install all modules
 source "$REPO_INSTALL/helpers/all.sh"
 source "$REPO_INSTALL/packages/all.sh"
 source "$REPO_INSTALL/system/all.sh"
