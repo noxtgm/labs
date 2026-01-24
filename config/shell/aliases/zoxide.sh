f62776814c299856cb8aec367166616cd19ad840
noxtgm <contact@noxtgm.me>
2026-01-24 14:15:50 -0800

    Fix zoxide directory not found error

--- config/shell/aliases/zoxide.sh
+++ config/shell/aliases/zoxide.sh
@@ -13,6 +13,6 @@ zd() {
   elif [ -d "$1" ]; then
     builtin cd "$1"
   else
-    z "$@" && printf "\U000F17A9 " && pwd || log_error "Directory not found."
+    echo "$@" && printf "\U000F17A9 " && pwd || echo "Directory not found."
   fi
 }
