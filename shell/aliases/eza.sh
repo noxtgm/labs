d99908666bbe227650bea24e7889cfc984128e6c
noxtgm <contact@noxtgm.me>
2026-02-08 15:19:07 -0800

    Update aliases

--- shell/aliases/eza.sh
+++ shell/aliases/eza.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Files
+# List
 alias lsx='eza -l --no-user --group-directories-first --icons=always --time-style=+"%Y-%m-%d %H:%M"'
 alias lsxa='lsx -a'
 alias ls='lsx --no-permissions --no-filesize'
 alias lsa='ls -a'
 
 # Tree
-alias treex='lsx --tree --level=2'
+alias treex='eza -l --no-user --group-directories-first --icons=always --time-style=+"%Y-%m-%d %H:%M" --tree --level=2'
 alias treexa='treex -a'
 alias tree='treex --no-permissions --no-filesize'
 alias treea='tree -a'
